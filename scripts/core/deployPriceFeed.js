80f1caf25eaffc6d9f31f88c778c9e519df33e8e
potvik <yuriy@harmony.one>
2024-07-09 00:41:23 +0300

    deploy price feed contracts

--- scripts/core/deployPriceFeed.js
+++ scripts/core/deployPriceFeed.js
@@ -5,27 +5,41 @@ const { toUsd } = require("../../test/shared/units")
 const network = (process.env.HARDHAT_NETWORK || 'mainnet');
 const tokens = require('./tokens')[network];
 
-async function getArbValues(signer) {
-  const { btc, eth, usdc, link, uni, usdt, mim, frax, dai } = tokens
-  const tokenArr = [btc, eth, usdc, link, uni, usdt, mim, frax, dai]
-  const fastPriceTokens = [btc, eth, link, uni]
+const priceFeedAddressWONE = "0x2C5E75a0d4ad94961EBEf4bFCFDEe284B7BD18c6";
+const woneTokenAddress = "0xcF664087a5bB0237a0BAd6742852ec6c8d69A27a";
 
-  const priceFeedTimelock = { address: "0x7b1FFdDEEc3C4797079C7ed91057e399e9D43a8B" }
+const wallet = {}
 
-  const updater1 = { address: "0x18eAc44875EC92Ed80EeFAa7fa7Ac957b312D366" }
-  const updater2 = { address: "0x2eD9829CFF68c7Bb40812f70c4Fc06A4938845de" }
-  const keeper1 = { address: "0xbEe27BD52dB995D3c74Dc11FF32D93a1Aad747f7" }
-  const keeper2 = { address: "0x94577665926885f47ddC1Feb322bc51470daA8E8" }
-  const updaters = [updater1.address, updater2.address, keeper1.address, keeper2.address]
+async function getHarmonyValues(signer) {
+  const wone = {
+    name: "wone",
+    address: woneTokenAddress,
+    decimals: 18,
+    priceFeed: priceFeedAddressWONE,
+    fastPricePrecision: 100000000,
+    maxCumulativeDeltaDiff: 250,
+    priceDecimals: 8,
+    isStrictStable: false
+  };
 
-  const tokenManager = { address: "0x2c247a44928d66041D9F7B11A69d7a84d25207ba" }
+  const [deployer] = await ethers.getSigners()
+  wallet.address = deployer.address
 
-  const positionRouter = await contractAt("PositionRouter", "0xb87a436B93fFE9D75c5cFA7bAcFff96430b09868")
+  const tokenArr = [wone]
+  const fastPriceTokens = [wone]
 
-  const fastPriceEvents = await contractAt("FastPriceEvents", "0x4530b7DE1958270A2376be192a24175D795e1b07", signer)
-  // const fastPriceEvents = await deployContract("FastPriceEvents", [])
+  const priceFeedTimelock = { address: "0xC5837D0acFbA7b6EECD964C18ff1DeCCC9b57A35" }
 
-  const chainlinkFlags = { address: "0x3C14e07Edd0dC67442FA96f1Ec6999c57E810a83" }
+  const updaters = [wallet.address]
+
+  const tokenManager = { address: wallet.address }
+
+  const positionRouter = await contractAt("PositionRouter", "0xf3f6484E2C01961Eb0Fd4217a5d7D57A791B7771")
+      
+  // const fastPriceEvents = await contractAt("FastPriceEvents", "0x4530b7DE1958270A2376be192a24175D795e1b07", signer)
+  const fastPriceEvents = await deployContract("FastPriceEvents", [])
+
+  const chainlinkFlags = null;
 
   return {
     fastPriceTokens,
@@ -39,50 +53,13 @@ async function getArbValues(signer) {
   }
 }
 
-async function getAvaxValues(signer) {
-  const { avax, btc, btcb, eth, mim, usdce, usdc } = tokens
-  const tokenArr = [avax, btc, btcb, eth, mim, usdce, usdc]
-  const fastPriceTokens = [avax, btc, btcb, eth]
-
-  const priceFeedTimelock = { address: "0xCa8b5F2fF7B8d452bE8972B44Dc026Be96b97228" }
-
-  const updater1 = { address: "0x2b249Bec7c3A142431b67e63A1dF86F974FAF3aa" }
-  const updater2 = { address: "0x63ff41E44d68216e716d236E2ECdd5272611D835" }
-  const keeper1 = { address: "0x5e0338CE6597FCB9404d69F4286194A60aD442b7" }
-  const keeper2 = { address: "0x8CD98FF48831aa8864314ae8f41337FaE9941C8D" }
-  const updaters = [updater1.address, updater2.address, keeper1.address, keeper2.address]
-
-  const tokenManager = { address: "0x9bf98C09590CeE2Ec5F6256449754f1ba77d5aE5" }
-
-  const positionRouter = await contractAt("PositionRouter", "0xffF6D276Bc37c61A23f06410Dce4A400f66420f8")
-
-  // const fastPriceEvents = await deployContract("FastPriceEvents", [])
-  const fastPriceEvents = await contractAt("FastPriceEvents", "0x02b7023D43bc52bFf8a0C54A9F2ecec053523Bf6", signer)
-
-  return {
-    fastPriceTokens,
-    fastPriceEvents,
-    tokenManager,
-    positionRouter,
-    tokenArr,
-    updaters,
-    priceFeedTimelock
-  }
-}
-
 async function getValues(signer) {
-  if (network === "arbitrum") {
-    return getArbValues(signer)
-  }
-
-  if (network === "avax") {
-    return getAvaxValues(signer)
-  }
+    return getHarmonyValues(signer)
 }
 
 async function main() {
   const signer = await getFrameSigner()
-  const deployer = { address: "0x5F799f365Fa8A2B60ac0429C48B153cA5a6f0Cf8" }
+  const deployer = { address: "0xcDF2A6446cd43B541fC768195eFE1f82c846F953" }
 
   const {
     fastPriceTokens,
@@ -96,24 +73,25 @@ async function main() {
   } = await getValues(signer)
 
   const signers = [
-    "0x82429089e7c86B7047b793A9E7E7311C93d2b7a6", // coinflipcanada
-    "0x1D6d107F5960A66f293Ac07EDd08c1ffE79B548a", // G Account 1
-    "0xD7941C4Ca57a511F21853Bbc7FBF8149d5eCb398", // G Account 2
-    "0xfb481D70f8d987c1AE3ADc90B7046e39eb6Ad64B", // kr
-    "0x99Aa3D1b3259039E8cB4f0B33d0Cfd736e1Bf49E", // quat
-    "0x6091646D0354b03DD1e9697D33A7341d8C93a6F5", // xhiroz
-    "0x45e48668F090a3eD1C7961421c60Df4E66f693BD", // Dovey
-    "0x881690382102106b00a99E3dB86056D0fC71eee6", // Han Wen
-    "0x2e5d207a4c0f7e7c52f6622dcc6eb44bc0fe1a13" // Krunal Amin
+    "0xcDF2A6446cd43B541fC768195eFE1f82c846F953"
+    // "0x82429089e7c86B7047b793A9E7E7311C93d2b7a6", // coinflipcanada
+    // "0x1D6d107F5960A66f293Ac07EDd08c1ffE79B548a", // G Account 1
+    // "0xD7941C4Ca57a511F21853Bbc7FBF8149d5eCb398", // G Account 2
+    // "0xfb481D70f8d987c1AE3ADc90B7046e39eb6Ad64B", // kr
+    // "0x99Aa3D1b3259039E8cB4f0B33d0Cfd736e1Bf49E", // quat
+    // "0x6091646D0354b03DD1e9697D33A7341d8C93a6F5", // xhiroz
+    // "0x45e48668F090a3eD1C7961421c60Df4E66f693BD", // Dovey
+    // "0x881690382102106b00a99E3dB86056D0fC71eee6", // Han Wen
+    // "0x2e5d207a4c0f7e7c52f6622dcc6eb44bc0fe1a13" // Krunal Amin
   ]
 
-  if (fastPriceTokens.find(t => !t.fastPricePrecision)) {
-    throw new Error("Invalid price precision")
-  }
+  // if (fastPriceTokens.find(t => !t.fastPricePrecision)) {
+  //   throw new Error("Invalid price precision")
+  // }
 
-  if (fastPriceTokens.find(t => !t.maxCumulativeDeltaDiff)) {
-    throw new Error("Invalid price maxCumulativeDeltaDiff")
-  }
+  // if (fastPriceTokens.find(t => !t.maxCumulativeDeltaDiff)) {
+  //   throw new Error("Invalid price maxCumulativeDeltaDiff")
+  // }
 
   const secondaryPriceFeed = await deployContract("FastPriceFeed", [
     5 * 60, // _priceDuration
@@ -149,7 +127,7 @@ async function main() {
       token.address, // _token
       token.priceFeed, // _priceFeed
       token.priceDecimals, // _priceDecimals
-      token.isStrictStable // _isStrictStable
+      token.isStrictStable // _table
     ), `vaultPriceFeed.setTokenConfig(${token.name}) ${token.address} ${token.priceFeed}`)
   }
 
